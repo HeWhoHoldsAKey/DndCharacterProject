6c46964b53717add835e0f93e760f878d0034a06
clint59876 <clintnichols2003@gmail.com>
2019-11-11 17:02:37 -0600

    It runs now
    Ok the last version was a little experiment but not it runs great... except it looks disgusting but ill fix that later.

--- projectCreation/src/projectCreation/characterDeclaring.java
+++ projectCreation/src/projectCreation/characterDeclaring.java
@@ -4,141 +4,105 @@ import java.util.*;
 public class characterDeclaring {
 
 	public static void main(String[] args) {
-		//a-dex , b-con, c-str, d-wis, e-int, f-cha
-        
-		int minNum = 3;
-		int maxNum = 18;
-		
-		
-		int dex = getRandomNum(minNum, maxNum);
-		int con = getRandomNum(minNum, maxNum);
-		int str = getRandomNum(minNum, maxNum);
-		int wis = getRandomNum(minNum, maxNum);
-		int inte = getRandomNum(minNum, maxNum);
-		int cha = getRandomNum(minNum, maxNum);
-		        
-		
-		String myClassName = getCharacter(dex, con, str, wis, inte, cha);
-		
+		public static void main(String[] args) {
+    	int minNum = 3;
+    	int maxNum = 18;
+
+    	int dex = getRandomNum(minNum, maxNum);
+    	int con = getRandomNum(minNum, maxNum);
+    	int str = getRandomNum(minNum, maxNum);
+    	int wis = getRandomNum(minNum, maxNum);
+    	int inte = getRandomNum(minNum, maxNum);
+    	int cha = getRandomNum(minNum, maxNum);
+
+    	String myClass = getCharacter(dex, con, str, wis, inte, cha);
+    	String myDice = getDice(myClass);
 		System.out.println("Dexterity: " + dex + "  |  Constitution: " + con + "  |  Strength: " + str + "  |  Wisdom: " + wis + "  |  Intelligence: " + inte + "  |  Charisma: " + cha);
-        System.out.println("Class: " + myClassName + "  |  Dice: " + characterObj.getDieReq());
+		System.out.println("Class: " + myClass);
+		System.out.println("Hit point dice: " + myDice);
 	}
-	
-	
-	//Makes a random number
-	public static int getRandomNum(int A, int B) {
-		Random rand = new Random();
-        return rand.nextInt((B - A) + 1) + A;
+
+
+
+	public static int getRandomNum(int min, int max) {
+    	Random rand = new Random();
+    	return rand.nextInt((max - min) + 1) + min;
 	}
-	
-	
-	//By 2 layer nested if statements we find the character...
+
+
+
+
+
 	public static String getCharacter(int a, int b, int c, int d, int e, int f) {
-		String classDecided = "";
-		if (a >= 15){
-
-            //rogue, monk
-            if (e > b){
-            	classDecided = "Rogue";
-            	//classDecided[1] = "D8";
-            }
-            else{
-            	classDecided = "Monk";
-            	//classDecided[1] = "D8";
-            }
-
-        }
-
-        else if (c >= 15){
-            //barb
-        	classDecided = "Barbarian";
-        	//classDecided[1] = "D12";
-        }
-        else if (d >= 15){
-            //druid, ranger dex, cleric con
-            if ((d + a) >= 25){
-            	classDecided = "Ranger";
-            	//classDecided[1] = "D10";
-            }
-            else if ((d + b) >= 25){
-            	classDecided = "Cleric";
-            	//classDecided[1] = "D8";
-            }
-            else{
-            	classDecided = "Druid";
-            	//classDecided[1] = "D8";
-            }
-        }
-        else if (e >= 15){
-            //wizard
-        	classDecided = "Wizard";
-        	//classDecided[1] = "D6";
-        }
-        else if (f >= 15){
-            //bard dex, sorcerer, warlock wis, paladin con
-            if ((f+a) >= 25){
-            	classDecided = "Bard";
-            	//classDecided[1] = "D8";
-            }
-            else if ((f+d) >= 25){
-            	classDecided = "Warlock";
-            	//classDecided[1] = "D8";
-            }
-                else if ((f+b) >= 25){
-                	classDecided = "Paladin";
-                	//classDecided[1] = "D10";
-            }
-            else {
-            	classDecided = "Sorcerer";
-            	//classDecided[1] = "D6";
-            }
-        }
-        else {
-            //fighter
-        	classDecided = "Fighter";
-        	//classDecided[1] = "D10";
-        }
-
-		return classDecided;
+    	String classDecided = "";
+    	if (a >= 15) {
+        	//rogue, monk
+        	if (e > b) {
+            	classDecided = "Rogue";//d8
+        	} else {
+            	classDecided = "Monk";//d8
+        	}
+    	} else if (c >= 15) {
+        	//barb
+        	classDecided = "Barbarian";//d12
+    	} else if (d >= 15) {
+        	//druid, ranger dex, cleric con
+        	if ((d + a) >= 25) {
+            	classDecided = "Ranger";//d10
+        	} else if ((d + b) >= 25) {
+            	classDecided = "Cleric";//d8
+        	} else {
+            	classDecided = "Druid";//d8
+        	}
+    	} else if (e >= 15) {
+        	//wizard
+        	classDecided = "Wizard";//d6
+    	} else if (f >= 15) {
+        	//bard dex, sorcerer, warlock wis, paladin con
+        	if ((f + a) >= 25) {
+            	classDecided = "Bard";//d8
+        	} else if ((f + d) >= 25) {
+            	classDecided = "Warlock";//d8
+        	} else if ((f + b) >= 25) {
+            	classDecided = "Paladin";//d10
+        	} else {
+            	classDecided = "Sorcerer";//d6
+        	}
+    	} else {
+        	//fighter
+        	classDecided = "Fighter";//d10
+    	}
+    	return classDecided;
 	}
 
-	//character classes :')
-	//gonna use rogue as our example puppet...
-	public class DNDmother {
-		private String dieReq = "D8";
-		public String getDieReq() {
-			return dieReq;
-		}
-	}
-	
-	public class Rogue extends DNDmother{
-		
-	}
-	public class Cleric extends DNDmother{
-		
-	}
-	public class Druid extends DNDmother{
-		
-	}
-	public class Wizard extends DNDmother{
-		
-	}
-	public class Bard extends DNDmother{
-		
-	}
-	public class Warlock extends DNDmother{
-		
-	}
-	public class Paladin extends DNDmother{
-		
-	}
-	public class Sorcerer extends DNDmother{
-		
-	}
-	public class Fighter extends DNDmother{
-		
-	}
 
-	
-	
+	public static String getDice(String myClass) {
+    	String diceDecided = "";
+    	if (myClass == "Rogue") {
+			diceDecided = "d8";
+    	} else if (myClass == "Monk") {
+  			diceDecided = "d8";
+    	} else if (myClass == "Barbarian") {
+			diceDecided = "d12";
+    	} else if (myClass == "Ranger") {
+			diceDecided = "d10";
+    	} else if (myClass == "Cleric") {
+			diceDecided = "d8";
+    	} else if (myClass == "Druid") {
+			diceDecided = "d8";
+    	} else if (myClass == "Wizard") {
+			diceDecided = "d6";
+    	} else if (myClass == "Bard") {
+			diceDecided = "d8";
+    	} else if (myClass == "Warlock") {
+			diceDecided = "d8";
+    	} else if (myClass == "Paladin") {
+			diceDecided = "d10";
+    	} else if (myClass == "Sorcerer") {
+			diceDecided = "d6";
+    	} else {
+			diceDecided = "d10";
+    	}
+    	return diceDecided;
+	}
 }
