71e65441af4b164f1e800d4b658763e145ff0891
Joshua-Nolen <57687387+Joshua-Nolen@users.noreply.github.com>
2019-11-12 15:22:40 -0600

    You are a bigger loser
    Clint

--- projectCreation/src/projectCreation/motherFile.java
+++ projectCreation/src/projectCreation/motherFile.java
@@ -1,1 +1,13 @@
+package projectCreation;
+
+public class motherFile {
+	public static void main(String[] args) {
+		
+		characterClass characterObj = new characterClass();
+			
+		characterObj.printInfo();
+		System.out.println("Character name is: " + characterObj.getName());
+
+	}
+}
 
