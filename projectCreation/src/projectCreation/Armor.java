03ae3330648ec11ef060c80cb0e3fc4421ff127c
Joshua-Nolen <57687387+Joshua-Nolen@users.noreply.github.com>
2019-11-18 15:24:04 -0600

    Update and rename Armor to Armor.java

---
+++ projectCreation/src/projectCreation/Armor.java
@@ -0,0 +1,11 @@
+package projectCreation;
+
+public class Armor{
+      public static void main(String[] args){
+        myClass = "Rogue";
+        myArmor = getArmor();
+        System.out.println("AC:  " + myArmor);
+      }
+    public class 
+    
+}
