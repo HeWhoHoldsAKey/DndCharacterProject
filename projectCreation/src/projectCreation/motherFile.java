05fe6b6a2b3fca38e8fe9831b51f517a4e947f5d
clint59876 <clintnichols2003@gmail.com>
2019-11-27 22:50:14 -0600

    Um...
    So. i uh, i somehow managed to change the class path to jdk 13.0.1 soooo yeah. Also prep for the ui has begun... idk at this point

--- projectCreation/src/projectCreation/motherFile.java
+++ projectCreation/src/projectCreation/motherFile.java
@@ -1,11 +1,35 @@
 package projectCreation;
 
+import java.util.Scanner;
+
 public class motherFile {
 	public static void main(String[] args) {
-		
+
+		Scanner input = new Scanner(System.in);
 		characterMain characterObj = new characterMain();
+
+		System.out.println("Would you like a random character, or would you like to build it your self?");
+		System.out.println("1. Random");
+		System.out.println("2. Build it yourself");
+		System.out.print(">>");
+		int ans = input.nextInt();
+
 		
-		characterObj.setClass(setCharacterClass.createCharacterClass(setCharacterClass.getDex(), setCharacterClass.getCon(), setCharacterClass.getStr(), setCharacterClass.getWis(), setCharacterClass.getInte(), setCharacterClass.getCha()));
-		characterObj.printInfo();
+
+		if (ans == 1) {
+			makeBarrier();
+			characterObj.setClass(setCharacterClass.createCharacterClass(setCharacterClass.getDex(),
+					setCharacterClass.getCon(), setCharacterClass.getStr(), setCharacterClass.getWis(),
+					setCharacterClass.getInte(), setCharacterClass.getCha()));
+			characterObj.printInfo();
+		} else {
+			makeBarrier();
+
+		}
+		input.close();
+	}
+
+	public static void makeBarrier() {
+		System.out.println("--------------------------------------------------------------------------------------");
 	}
 }
